761c51c504d1e101db5b31723d9f93cf88c69361
Andrew Almeida <andrew.almeida@PLASTI.local>
2023-10-04 07:23:41 -0300

    Exercise 02

---
+++ exercise_02.py
@@ -0,0 +1,8 @@
+# Make a program that ask for a number and then show the message "The number is: {numver}"
+
+def showNumber():
+    askedNumber = input('Type a number: ')
+    print('The number is {}'.format(askedNumber))
+
+showNumber()
+
