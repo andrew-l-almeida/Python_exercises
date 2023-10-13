3a635632e7983380cf1a87bcfb9877360cfbe3ba
Andrew Almeida <andrew.almeida@PLASTI.local>
2023-10-13 16:23:56 -0300

    Exercise 27 start

---
+++ exercise_27.py
@@ -0,0 +1,5 @@
+print("\033[31mTexto Vermelho\033[0m")
+print("\033[32mTexto Verde\033[0m")
+print("\033[33mTexto Amarelo\033[0m")
+print("\033[34mTexto Azul\033[0m")
+print("\033[35mTexto Roxo\033[0m")
